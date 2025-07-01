947695784c45a47a3f6c6878d6f5914e11aa125f
tamiopia <tamiopia23@gmail.com>
2025-07-01 22:38:40 +0300

    chore(app): configure MySQL, Swagger, and global modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '', // Or your MySQL password
+      database: 'blog_db',
+      entities: [User],
+      synchronize: true, // Dev only
+    }),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Clean Architecture Blog with JWT Auth and CQRS')
+    .setVersion('1.0')
+    .addBearerAuth() // Enable JWT header
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // 👈 /api URL
+
+  await app.listen(3000);
 }
 bootstrap();
