6fa826320ff20d5eb94b0d5abd6b3f89806e0b5f
tamiopia <tamiopia23@gmail.com>
2025-07-02 21:58:54 +0300

    chore(posts): add UpdatePostDto

---
+++ src/posts/dtos/update-post.dto.ts
@@ -0,0 +1,10 @@
+
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdatePostDto {
+  @ApiProperty({ required: false })
+  title?: string;
+
+  @ApiProperty({ required: false })
+  content?: string;
+}
