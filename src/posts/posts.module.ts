9bffb534ce5c280b0eb90ea8d395a58b0bc9f1ea
tamiopia <tamiopia23@gmail.com>
2025-07-02 21:58:55 +0300

    chore(posts): register handlers and queries in PostsModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -4,10 +4,21 @@ import { Post } from './entities/post.entity';
 import { PostsController } from './posts.controller';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreatePostHandler } from './commands/create-post.handler';
+import { GetPostWithCommentsHandler } from './queries/get-post-with-comments.handler';
+import { UpdatePostHandler } from './commands/update-post.handler';
+import { DeletePostHandler } from './commands/delete-post.handler';
+import { GetPostByIdHandler } from './queries/get-post-by-id.handler';
+import { GetPostsHandler } from './queries/get-posts.handler';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post]), CqrsModule],
   controllers: [PostsController],
-  providers: [CreatePostHandler],
+  providers: [CreatePostHandler,
+    GetPostWithCommentsHandler,
+    UpdatePostHandler,
+    DeletePostHandler,
+    GetPostByIdHandler,
+    GetPostsHandler
+  ],
 })
 export class PostsModule {}
