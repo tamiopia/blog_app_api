465bf116d002345f1ac570198b6c25190dc02b7b
tamiopia <tamiopia23@gmail.com>
2025-07-02 12:01:44 +0300

    chore(app): import PostsModule and register Post entity in TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
+import { Post } from './posts/entities/post.entity';
+import { PostsModule } from './posts/posts.module';
+import { JwtStrategy } from './auth/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
@@ -13,14 +20,20 @@ import { User } from './users/entities/user.entity';
       username: 'root',
       password: '', // Or your MySQL password
       database: 'blog_db',
-      entities: [User],
+      entities: [User,Post],
       synchronize: true, // Dev only
     }),
     TypeOrmModule.forFeature([User]),
     AuthModule,
     UsersModule,
+    PostsModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET , 
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
