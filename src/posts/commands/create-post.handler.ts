70b081572b7943a492894f3c199efe3ea01f6a5c
tamiopia <tamiopia23@gmail.com>
2025-07-02 23:54:38 +0300

    refactor(posts): use PostRepository abstraction in create-post handler

--- src/posts/commands/create-post.handler.ts
+++ src/posts/commands/create-post.handler.ts
@@ -1,24 +1,22 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Post } from '../entities/post.entity';
 import { CreatePostCommand } from './create-post.command';
+import { PostRepository } from '../repositories/post.repository';
+import { Post } from '../entities/post.entity';
 
 @CommandHandler(CreatePostCommand)
 export class CreatePostHandler implements ICommandHandler<CreatePostCommand> {
-  constructor(
-    @InjectRepository(Post)
-    private readonly postRepo: Repository<Post>,
-  ) {}
+  constructor(private readonly postRepo: PostRepository) {}
 
   async execute(command: CreatePostCommand): Promise<Post> {
-    const post = this.postRepo.create({
-      title: command.title,
-      content: command.content,
-      image: command.image,
-      userId: command.userId,
+    const { title, content, image, userId } = command;
+
+    const newPost = await this.postRepo.create({
+      title,
+      content,
+      image,
+      userId,
     });
 
-    return this.postRepo.save(post);
+    return newPost;
   }
 }
