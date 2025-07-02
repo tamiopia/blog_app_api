3a079d6280200a6c7c2b52e47709edd6c43b2a36
tamiopia <tamiopia23@gmail.com>
2025-07-02 11:59:11 +0300

    feat(posts): add CreatePostCommand to encapsulate post creation data

--- src/posts/commands/create-post.command.ts
+++ src/posts/commands/create-post.command.ts
@@ -1,7 +1,8 @@
 export class CreatePostCommand {
-    constructor(
-      public readonly title: string,
-      public readonly content: string
-    ) {}
-  }
-  
+  constructor(
+    public readonly title: string,
+    public readonly content: string,
+    public readonly image: string,
+    public readonly userId: string, // Add userId here
+  ) {}
+}
